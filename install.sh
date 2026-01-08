9f1a5c8cb2f1254737f3aaa73c43ec0ca7349fd3
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2026-01-08 18:01:01 +0000

    Fixed It.

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
+ARCH=$(uname -m)
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
 fi
-if [[ "$ARCH" == x86_64*]] then
+if [[ "$ARCH" == x86_64* ]]; then
 wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/diginum -O /usr/bin/diginum
 wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/sand-setplat -O /usr/bin/sand-setplat
 fi
-if [[ "$ARCH" == aarch64*]] then
+if [[ "$ARCH" == aarch64* ]]; then
 wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/diginum_aarch64 -O /usr/bin/diginum
-wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/sand-setplat-aarch64 -O /usr/bin/sand-setplat
+wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/sand-setplat-aarch64 -O /usr/bin/sand>
 fi
 sand-setplat --no-override
 rm -f /usr/bin/sand-setplat
-wget [ICON] -O /usr/share/sandpotnoodles/diginum.png
-wget [desktop-file] -O /usr/share/applications/diginum.desktop
-
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/diginum/refs/heads/main/diginum.png -O /usr/share/sandpotnoodles/diginum.png
+wget https://github.com/ActuallySandPotNoodles/diginum/blob/main/diginum.desktop -O /usr/share/applications/diginum.desktop
