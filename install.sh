79f3344adb3c51731139df8897947740da356bbb
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2026-06-16 14:25:35 +0100

    Rename diginum to diginum-py in install script

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 if [[ "$ARCH" == x86_64* ]]; then
-wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/diginum -O /usr/bin/diginum
+wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/diginum -O /usr/bin/diginum-py
 wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/sand-setplat -O /usr/bin/sand-setplat
 fi
 if [[ "$ARCH" == aarch64* ]]; then
-wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/diginum_aarch64 -O /usr/bin/diginum
+wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/diginum_aarch64 -O /usr/bin/diginum-py
 wget https://github.com/ActuallySandPotNoodles/diginum/releases/download/Reease-1.0.0/sand-setplat-aarch64 -O /usr/bin/sand-setplat
 fi
 chmod 775 /usr/bin/sand-setplat
@@ -17,4 +17,4 @@ mkdir /usr/share/sandpotnoodles
 sand-setplat --no-override
 rm -f /usr/bin/sand-setplat
 wget https://raw.githubusercontent.com/ActuallySandPotNoodles/diginum/refs/heads/main/diginum.png -O /usr/share/sandpotnoodles/diginum.png
-wget https://raw.githubusercontent.com/ActuallySandPotNoodles/diginum/refs/heads/main/diginum.desktop -O /usr/share/applications/diginum.desktop
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/diginum/refs/heads/main/diginum.desktop -O /usr/share/applications/diginum-py.desktop
